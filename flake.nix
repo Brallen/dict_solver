b37f9ad0535ba44194e865ae63696af31d875665
Brett <24304042+Brallen@users.noreply.github.com>
2026-01-16 16:57:20 -0800

    Adding in nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Gleam development environment";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        unstable = nixpkgs-unstable.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            unstable.gleam
+            erlang
+            rebar3 # For erlang
+	    inotify-tools # For file watching
+          ];
+
+          shellHook = ''
+            echo "Gleam development environment loaded!"
+            echo "Gleam version: $(gleam --version)"
+            echo "Erlang version: $(erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().' -noshell)"
+          '';
+        };
+      }
+    );
+}
